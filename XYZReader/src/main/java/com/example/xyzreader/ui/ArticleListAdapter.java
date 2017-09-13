9f78f42f7f687c21e4f57846c76e8306922bb97b
Jawad Hassan Soomro <jawadhassansoomro@gmail.com>
2017-09-14 03:41:40 +0500

    Worked on Cursor Adapter

---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListAdapter.java
@@ -0,0 +1,107 @@
+package com.example.xyzreader.ui;
+
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.example.xyzreader.R;
+import com.example.xyzreader.data.ArticleLoader;
+import com.example.xyzreader.data.ItemsContract;
+
+public class ArticleListAdapter extends CursorRecyclerViewAdapter<ArticleListAdapter.ViewHolder>{
+
+
+    public ArticleListAdapter(Cursor cursor) {
+        super(cursor);
+
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_article, parent, false);
+        final ViewHolder vh = new ViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                parent.getContext().startActivity(new Intent(Intent.ACTION_VIEW,
+                        ItemsContract.Items.buildItemUri(getItemId(vh.getAdapterPosition()))));
+            }
+        });
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final Cursor cursor) {
+        holder.titleView.setText(cursor.getString(ArticleLoader.Query.TITLE));
+        holder.subtitleView.setText(
+                DateUtils.getRelativeTimeSpanString(
+                        cursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
+                        System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
+                        DateUtils.FORMAT_ABBREV_ALL).toString());
+        holder.authorView.setText(cursor.getString(ArticleLoader.Query.AUTHOR));
+        holder.thumbnailView.setAspectRatio(cursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+        Glide.with(holder.thumbnailView.getContext())
+                .load(cursor.getString(ArticleLoader.Query.THUMB_URL))
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        Bitmap bitmap = ((BitmapDrawable) resource.getCurrent()).getBitmap();
+                        Palette palette = Palette.generate(bitmap);
+                        int defaultColor = 0xFF333333;
+                        int color = palette.getDarkMutedColor(defaultColor);
+                        holder.itemView.setBackgroundColor(color);
+                        return false;
+                    }
+                })
+                .into(holder.thumbnailView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return super.getItemCount();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public DynamicHeightNetworkImageView thumbnailView;
+        public TextView titleView;
+        public TextView subtitleView;
+        public TextView authorView;
+
+
+
+        public ViewHolder(View view) {
+            super(view);
+            thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.thumbnail);
+            titleView = (TextView) view.findViewById(R.id.article_title);
+            subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
+            authorView = (TextView) view.findViewById(R.id.article_author);
+        }
+    }
+}
+
+
+
